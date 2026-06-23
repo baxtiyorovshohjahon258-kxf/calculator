af3a7b26adef913b32a9ed41f79ac86c2e615992
baxtiyorovshohjahon258-kxf <baxtiyorovshohjahon258@gmail.com>
2026-06-23 12:24:25 +0300

    por vs uzb

--- Paper.py
+++ Paper.py
@@ -1,37 +1,3 @@
-# while True:
-#     print("\n1. Yig'indini hisoblash")
-#     print("2. Ayirmani hisoblash")
-#     print("3. Ko'paytmani hisoblash")
-#     print("4. Bo'linmani hisoblash")
-#     print("5. Chiqish")
-
-#     tanlov = input("Tanlang (1-5): ")
-
-#     if tanlov == "5":
-#         print("Foydalanganingiz uchun rahmat!")
-#         break
-
-#     A = int(input("Birinchi sonni kiriting: "))
-#     B = int(input("Ikkinchi sonni kiriting: "))
-
-#     if tanlov == "1":
-#         print("Natija:", A + B)
-
-#     elif tanlov == "2":
-#         print("Natija:", A - B)
-
-#     elif tanlov == "3":
-#         print("Natija:", A * B)
-
-#     elif tanlov == "4":
-#         if B != 0:
-#             print("Natija:", A / B)
-#         else:
-#             print("0 ga bo'lish mumkin emas!")
-
-#     else:
-#         print("Noto'g'ri tanlov!")
-
 while True:
         print("\n1. Yig'indini hisoblash")
         print("2. Ayirmani hisoblash")
---
+++ def.py
@@ -0,0 +1,96 @@
+def salom_ber(ism):
+    for i in range(ism):
+        print("salom" , ism)
+        print(i)
+salom_ber(1)
+
+def vazifa(name, year, fakultet):
+    print(f"Foydalanuvchi ismi: {name} \nFoydalanuvchi yoshi {2026 - year} \nTug'ilgan yili: {year} \nMutaxasisligi {fakultet}")
+
+vazifa("Shohjahon" , 2008 , "Cybersecurity")
+
+vazifa("Shaxzod" , 2005 , "Cybersecurity")
+
+vazifa("Amir Temur" ,1336  , "Qirol , Sarkarda")
+
+vazifa("Alisher Navoiy " ,1441 ,"Yozuvchi")
+# vazifa("" ,  , "")
+
+
+def vazifa(name, year, fakultet):
+    print(f"Foydalanuvchi ismi: {name} \nFoydalanuvchi yoshi {2026 - year} \nTug'ilgan yili: {year} \nMutaxasisligi {fakultet}")
+
+vazifa("Shohjahon" , 2008 , "Cybersecurity")
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+# =====1-misol==== #
+
+# def salom_ber(ism):
+#     print("Salom", ism)
+# salom_ber("Shohjahon")
+# # =====2-misol==== #
+
+# def born_year(year):
+#     print(f"\nTug'ilgan yilingiz: {year} \nsizningiz yoshingiz {2026-year}")
+# born_year(2008)
+# # =====3-misol==== #
+
+# def yigindisi(a, b , c , d):
+#     print(a + b + c + d)
+# yigindisi(63, 104,  11, 11)
+# # =====4-misol==== #
+
+# def ayirma(a , b):
+#     if a > b and a > 0 and b >= 0 or b <= 0:
+#         print("musbat natija")
+#     elif a < b :
+#         print(a - b)
+# ayirma(10, 52)
+# # =====5-misol==== #
+
+# def kvadrat(son):
+#     print(son**2)
+# kvadrat(10)
+# # =====6-misol==== #
+
+# def taqqoslash(son1 , son2):
+#     if son1 > son2:
+#         print(son1, "soni katta")
+#     elif son1 < son2:
+#         print(son2, "soni katta")
+#     else:
+#         print("sonlar teng")
+# taqqoslash(10, 74)
+# taqqoslash(10, 10)
+# taqqoslash(74, 10)
+# # =====7-misol==== #
+
+# def juf_aniqlash(a , b):
+#     if a % 2 == 0 and b % 2 == 0:
+#         print(a, "va", b, "juft sonlar")
+#     elif a % 2 != 0 and b % 2 != 0:
+#         print(a, "va", b, "toq sonlar")
+#     elif a % 2 != 0 and b % 2 == 0:
+#         print(a, "toq son", b, "juft son")
+#     elif a % 2 == 0 and b % 2 != 0:
+#         print(a, "juft son", b, "toq son")
+# juf_aniqlash(21, 21)
+# juf_aniqlash(10, 15)
+# juf_aniqlash(11, 10)
+# juf_aniqlash(14, 10)
+
+    
+
+
+
+
+
+
+
+
+
